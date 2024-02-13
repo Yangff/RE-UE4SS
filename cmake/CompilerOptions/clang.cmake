104916c631c646c8af223cf311d63096fc485ce2
Yangff <yangff1@gmail.com>
2024-02-12 19:56:43 -0600

    fix compiler flag for linux

--- cmake/CompilerOptions/clang.cmake
+++ cmake/CompilerOptions/clang.cmake
@@ -1,4 +1,8 @@
-set(DEFAULT_COMPILER_FLAGS "-g;-gcodeview;-fcolor-diagnostics;-Wno-unknown-pragmas;-Wno-unused-parameter;-fms-extensions;-Wignored-attributes" PARENT_SCOPE)
+if (WIN32)
+    set(DEFAULT_COMPILER_FLAGS "-g;-gcodeview;-fcolor-diagnostics;-Wno-unknown-pragmas;-Wno-unused-parameter;-fms-extensions;-Wignored-attributes" PARENT_SCOPE)
+else()
+    set(DEFAULT_COMPILER_FLAGS "-g;-fcolor-diagnostics;-Wno-unknown-pragmas;-Wno-unused-parameter;-fms-extensions;-Wignored-attributes" PARENT_SCOPE)
+endif()
 
 set(LINKER_FLAGS "-g" PARENT_SCOPE)
 set(DEFAULT_SHARED_LINKER_FLAGS "${LINKER_FLAGS}" PARENT_SCOPE)
