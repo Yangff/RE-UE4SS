030e166edcef523b12f86b021f7638e66b03d467
Yangff <yangff1@gmail.com>
2024-02-16 02:42:52 -0600

    force disable LTO on CI?

--- cmake/CompilerOptions/clang.cmake
+++ cmake/CompilerOptions/clang.cmake
@@ -2,6 +2,7 @@ if (WIN32)
     set(DEFAULT_COMPILER_FLAGS "-g;-gcodeview;-fcolor-diagnostics;-Wno-unknown-pragmas;-Wno-unused-parameter;-fms-extensions;-Wignored-attributes" PARENT_SCOPE)
 else()
     set(DEFAULT_COMPILER_FLAGS "-g;-fcolor-diagnostics;-Wno-unknown-pragmas;-Wno-unused-parameter;-fms-extensions;-Wignored-attributes" PARENT_SCOPE)
+    set(CMAKE_INTERPROCEDURAL_OPTIMIZATION OFF)
 endif()
 
 set(LINKER_FLAGS "-g" PARENT_SCOPE)
