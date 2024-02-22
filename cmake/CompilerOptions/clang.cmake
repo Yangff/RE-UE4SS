05808d2c5407bbcfa672563274a7e3bfa31b23d9
Yangff <yangff1@gmail.com>
2024-02-22 04:47:56 -0600

    add -fno-delete-null-pointer-checks to allow this == nullptr

--- cmake/CompilerOptions/clang.cmake
+++ cmake/CompilerOptions/clang.cmake
@@ -1,7 +1,7 @@
 if (WIN32)
     set(DEFAULT_COMPILER_FLAGS "-g;-gcodeview;-fcolor-diagnostics;-Wno-unknown-pragmas;-Wno-unused-parameter;-fms-extensions;-Wignored-attributes" PARENT_SCOPE)
 else()
-    set(DEFAULT_COMPILER_FLAGS "-g;-fcolor-diagnostics;-Wno-unknown-pragmas;-Wno-unused-parameter;-fms-extensions;-Wignored-attributes" PARENT_SCOPE)
+    set(DEFAULT_COMPILER_FLAGS "-g;-fcolor-diagnostics;-Wno-unknown-pragmas;-Wno-unused-parameter;-fms-extensions;-Wignored-attributes;-fno-delete-null-pointer-checks" PARENT_SCOPE)
     set(CMAKE_INTERPROCEDURAL_OPTIMIZATION OFF)
 endif()
 
