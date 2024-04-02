2235e8124eaf2a36a4ebd946c247b8e0728abc22
N00byKing <N00byKing@hotmail.de>
2024-04-02 23:04:47 +0200

    Simplify FileDef.hpp
    Equality checking in preprocessor seems to be quite wonky

--- deps/first/File/include/File/FileDef.hpp
+++ deps/first/File/include/File/FileDef.hpp
@@ -2,37 +2,29 @@
 
 namespace RC::File
 {
+
 #ifndef RC_DETECTED_OS
 #ifdef WIN32
 #ifdef _WIN32
 #define RC_DETECTED_OS _WIN32
+#ifndef RC_OS_FILE_TYPE_INCLUDE_FILE
+#define RC_OS_FILE_TYPE_INCLUDE_FILE <File/FileType/WinFile.hpp>
+#endif
 #else
-    static_assert(false, "Could not setup the 'Handle' typedef because a supported OS was not detected.");
+    static_assert(false, "Could not setup the 'RC_OS_FILE_TYPE_INCLUDE_FILE' macro because a supported OS was not detected.");
 #endif
 
 #else
 
 #ifdef __linux__
 #define RC_DETECTED_OS LINUX
-#else
-    static_assert(false, "Could not setup the 'Handle' typedef because a supported OS was not detected.");
-#endif
-
-#endif
-#endif
-
-#if RC_DETECTED_OS == __linux__
 #ifndef RC_OS_FILE_TYPE_INCLUDE_FILE
 #define RC_OS_FILE_TYPE_INCLUDE_FILE <File/FileType/StdFile.hpp>
+#endif
 #else
     static_assert(false, "Could not setup the 'RC_OS_FILE_TYPE_INCLUDE_FILE' macro because a supported OS was not detected.");
 #endif
-#endif
-#if RC_DETECTED_OS == _WIN32
-#ifndef RC_OS_FILE_TYPE_INCLUDE_FILE
-#define RC_OS_FILE_TYPE_INCLUDE_FILE <File/FileType/WinFile.hpp>
-#else
-    static_assert(false, "Could not setup the 'RC_OS_FILE_TYPE_INCLUDE_FILE' macro because a supported OS was not detected.");
+
 #endif
 #endif
 
