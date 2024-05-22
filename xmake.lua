300d8d43bcada86514dd4755842a0f83d87319de
localcc <work@localcc.cc>
2024-05-22 18:41:52 +0200

    fix xmake compilation

--- xmake.lua
+++ xmake.lua
@@ -81,5 +81,3 @@ end
 -- TODO: Remove this before the next release. It only exists to maintain backwards compat
 -- warnings for older mod templates.
 set_config("scriptsRoot", path.join(os.scriptdir(), "tools/xmakescripts"))
-
-add_repositories("thirdr deps/thirdr")
